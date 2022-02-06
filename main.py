0a14381a7632a3c1838bdeaf0c95a360939b92f7
Alex Volkov <47870166+23volkova@users.noreply.github.com>
2022-02-06 19:35:18 +0900

    added basic script for requesting data from api up do 5 days prior

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import requests, time
 def main():
-    print("Hello World!")
+    x, y, t = input("Enter the coords and time: ").split()
+    t = str(int(time.time() - int(t)))
+
+    #https: // api.openweathermap.org / data / 2.5 / onecall / timemachine?lat = {lat} & lon = {lon} & dt = {time} & appid = {APIkey}
+
+    api_key = 'b8a0167de4e3b9dd8c211ec3dd2f98f6'
+    response = requests.get("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + x +"&lon=" + y + "&dt=" + t + "&appid=b8a0167de4e3b9dd8c211ec3dd2f98f6")
+    data = response.json()
+    print(data)
 
 
 if __name__ == "__main__":
