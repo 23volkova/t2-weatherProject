ecc83673a18f105cec8de254e83481409b41b2ca
Alex Volkov <47870166+23volkova@users.noreply.github.com>
2022-02-07 18:54:39 +0900

    Added city mode, along with the rounding of Temperature for ease of use

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, time
 
 
-def dataRequest(x, y, t):
+def dataRequestCoord(x, y, t):
     # https: // api.openweathermap.org / data / 2.5 / onecall / timemachine?lat = {lat} & lon = {lon} & dt = {time} &
     # appid = {APIkey}
 
@@ -10,13 +10,28 @@ def dataRequest(x, y, t):
         "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + x + "&lon=" + y + "&dt=" + t + "&appid=" + api_key)
     data = response.json()
     print(data)
-    print("Temp: " + str(data['current']['temp']-273.15) + "C")
+    print("Temp: " + str(round(data['current']['temp']-273.15, 2)) + "C")
+
+
+def dataRequestCity(city, t):
+    # https: // api.openweathermap.org / data / 2.5 / onecall / timemachine?lat = {lat} & lon = {lon} & dt = {time} &
+    # appid = {APIkey}
+
+    api_key = 'b8a0167de4e3b9dd8c211ec3dd2f98f6'
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&dt=" + t + "&appid=" + api_key)
+    #response = requests.get(
+    #    "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + x + "&lon=" + y + "&dt=" + t + "&appid=" + api_key)
+    data = response.json()
+    print(data)
+    print("Temp: " + str(round(data['main']['temp']-273.15, 2)) + "C")
 
 
 def main():
-    x, y, t = input("Enter the coords and time: ").split()
+    #x, y, t = input("Enter the coords and time: ").split()
+    city, t = input("Enter the city name and time: ").split()
     t = str(int(time.time() - int(t)))
-    dataRequest(x, y, t)
+    #dataRequestCoord(x, y, t)
+    dataRequestCity(city, t)
 
 
 if __name__ == "__main__":
